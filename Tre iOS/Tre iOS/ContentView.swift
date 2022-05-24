4bc734a46ed34990ed416e059c5c266e823d6c06
Arttu Viljakainen <arttu.viljakainen@gmail.com>
2022-05-24 11:51:22 +0300

    .gitignore and hello world iOS app

---
+++ Tre iOS/Tre iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tre iOS
+//
+//  Created by Arttu Viljakainen on 24.5.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Tampere!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tre iOS/Tre iOS/Tre_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tre_iOSApp.swift
+//  Tre iOS
+//
+//  Created by Arttu Viljakainen on 24.5.2022.
+//
+
+import SwiftUI
+
+@main
+struct Tre_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
